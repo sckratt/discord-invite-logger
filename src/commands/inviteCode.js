9a4dccf71482d3b10120337874dc3f17075ba00f
aeziotech <aeziotech@gmail.com>
2020-11-09 19:15:12 +0100

    Fixing bugs in the 'inviteCode.js' file

--- src/commands/inviteCode.js
+++ src/commands/inviteCode.js
@@ -9,16 +9,13 @@ const { fromIntToLocalDate } = require('versus-tools');
  * @param {Array<string>} args 
  */
 const run = async (client, msg, args) => {
-    const member = args[0] ? msg.mentions.members.first() || msg.guild.members.cache.get(args[0]) : msg.member;
-    if(!member) return client.sendError("Aucun membre ne correspond aux informations donnée.", msg);
-
     try {
         const invites = (await msg.guild.fetchInvites())
-            .filter(i => i.inviter && i.inviter.id == member.user.id)
+            .filter(i => i.inviter && i.inviter.id == msg.author.id)
             .sort((a, b) => a.createdTimestamp - b.createdTimestamp)
             .array()
         let embed = new MessageEmbed()
-            .setColor(client.config.embedColor)
+            .setColor(client.config.embedColors)
             .setAuthor(`Vos invitations sur le serveur ${msg.guild.name}`, msg.guild.iconURL())
         let pages = [];
         let p = [];
@@ -39,44 +36,48 @@ const run = async (client, msg, args) => {
             for (let reaction of reactions) {
                 await message.react(reaction);
             };
+            
+            let page = 0;
+            let loop = true;
+            while (loop) {
+                embed.fields = []
+                embed
+                    .setFooter(`Page ${page+1}/${pages.length}`)
+                    .setDescription(
+                        pages[page].map(invite => {
+                            embed.addField(
+                                invite.code,
+                                `**Utilisations**: ${invite.uses}\n` +
+                                `**Expire dans**: ${invite.expiresTimestamp > 0 ? fromIntToLocalDate(invite.expiresTimestamp - Date.now()) : "∞"}\n` +
+                                `**Utilisations maximum**: ${invite.maxUses == 0 ? "∞" : invite.maxUses}\n` +
+                                `**Salon**: ${invite.channel ? invite.channel.toString() : `${client.emotes.get("no").toString()}`}`
+                            )
+                        })
+                    )
+                await message.edit(embed);
+                let filter = (reaction, user) => {
+                    return reactions.includes(reaction.emoji.name) && user.id == msg.author.id;
+                };
+                await message.awaitReactions(filter, { max: 1, time: 60000, errors: ["time"] }).then(async (collected) => {
+                    let reaction = collected.first();
+                    if(reaction.emoji.name == "⬅️" && page > 0) page--;
+                    else if(reaction.emoji.name == "➡️" && page < pages.length-1) page++;
+                }).catch(() => {
+                    message.delete();
+                    return loop = false;
+                });
+                
+                const userReactions = message.reactions.cache.filter(reaction => reaction.users.cache.has(msg.author.id));
+                try {
+                    for (const reaction of userReactions.values()) {
+                        await reaction.users.remove(msg.author.id);
+                    }
+                } catch {};
+            };
         } catch {
             return client.sendError("Je n'ai pas pu vous envoyer le message. Veuillez débloquer vos messages privés et réessayer.", msg).catch(()=>{});
         };
-
-        let page = 0;
-        let loop = true;
-        while (loop) {
-            embed
-                .setFooter(`Page ${page+1}/${pages.length}`)
-                .setDescription(
-                    pages[page].map(invite => {
-                        embed.addField(
-                            invite.code,
-                            `**Utilisations**: ${invite.uses}\n` +
-                            `**Durée maximum**: ${invite.maxAge > 0 ? fromIntToLocalDate(invite.maxAge) : "∞"}\n` +
-                            `**Utilisations maximum**: ${invite.maxUses == 0 ? "∞" : invite.maxUses}\n` +
-                            `**Salon**: ${invite.channel ? invite.channel.toString() : `${client.emotes.get("no").toString()}`}`
-                        )
-                    })
-                )
-            await message.edit(embed);
-            let filter = (reaction, user) => {
-                return reactions.includes(reaction.emoji.name) && user.id == msg.author.id;
-            };
-            await new Message().awaitReactions(filter, { max: 1, time: 60000, errors: ["time"] }).then(async (collected) => {
-                let reaction = collected.first();
-                if(reaction.emoji.name == "⬅️" && page > 0) page--;
-                else if(reaction.emoji.name == "➡️" && page < pages.length-1) page++;
-            });
-            
-            const userReactions = message.reactions.cache.filter(reaction => reaction.users.cache.has(msg.author.id));
-            try {
-                for (const reaction of userReactions.values()) {
-                    await reaction.users.remove(msg.author.id);
-                }
-            } catch {};
-        };
-    } catch {};
+    } catch (err) { console.error(err); };
 };
 
 module.exports = {
