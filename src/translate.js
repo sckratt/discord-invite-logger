976a0561e75b5e1cf355b550681b7d1f955b241a
Ae Zio-Tech <67607934+aeziotech@users.noreply.github.com>
2021-09-27 21:28:10 +0200

    Delete src directory

--- src/translate.js
+++
@@ -1,8 +0,0 @@
-/**
- * @param {string[]} texts
- */
-module.exports = function (...texts) {
-    let langs = [ "fr", "en" ];
-    let index = langs.indexOf(require('../config.json').lang.toLowerCase());
-    return texts[index];
-};
